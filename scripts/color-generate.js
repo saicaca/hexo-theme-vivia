eba839477b06cede208788672ffc824f84ed2b54
saica.go <zephyird@gmail.com>
2023-01-12 00:20:14 +0800

    Improve styles, rewrite page navigation bar

--- scripts/color-generate.js
+++ scripts/color-generate.js
@@ -3,7 +3,7 @@ const fs   = require('fs');
 const Color = require("colorjs.io").default;
 
 const config = yaml.load(fs.readFileSync('./themes/vix/_config.yml'));
-const stylus = require('stylus')
+const stylus = require('stylus');
 
 function getColorNode(l, c, h) {
     let str = new Color("oklch", [l, c, h]).to("srgb").toString({format: "hex"});
@@ -17,6 +17,7 @@ hexo.extend.filter.register('stylus:renderer', function(style) {
     style.define('color-L20', getColorNode(0.20, 0.020, hue))
     style.define('color-L25', getColorNode(0.25, 0.020, hue))
     style.define('color-L40', getColorNode(0.40, 0.040, hue))
+    style.define('color-L50', getColorNode(0.50, 0.060, hue))
     style.define('color-L75', getColorNode(0.75, 0.120, hue))
 })
   

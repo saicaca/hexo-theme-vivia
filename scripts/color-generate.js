e8b3eaca1d440664bbd6520f87aff1b0443e0539
saica.go <zephyird@gmail.com>
2023-01-26 11:24:31 +0800

    Refactor color system

--- scripts/color-generate.js
+++ scripts/color-generate.js
@@ -3,6 +3,7 @@ const fs   = require('fs');
 const Color = require("colorjs.io").default;
 
 const config = yaml.load(fs.readFileSync('./themes/vix/_config.yml'));
+const colors = require("../source/colors.json")
 const stylus = require('stylus');
 
 function getColorNode(l, c, h) {
@@ -12,12 +13,13 @@ function getColorNode(l, c, h) {
 
 hexo.extend.filter.register('stylus:renderer', function(style) {
     let hue = config.hue;
-    style.define('--main-bg-color', getColorNode(0.16, 0.015, hue))
-    style.define('color-L16', getColorNode(0.16, 0.015, hue))
-    style.define('color-L20', getColorNode(0.20, 0.020, hue))
-    style.define('color-L25', getColorNode(0.25, 0.020, hue))
-    style.define('color-L40', getColorNode(0.40, 0.040, hue))
-    style.define('color-L50', getColorNode(0.50, 0.060, hue))
-    style.define('color-L75', getColorNode(0.75, 0.120, hue))
+    for (let [theme, list] of Object.entries(colors)) {
+        for (let color of list) {
+            style.define(`${theme}-${color.name}`, getColorNode(color.l, color.c, hue));
+        }
+    }
 })
-  
+
+hexo.extend.helper.register('getColors', function() {
+    return colors;
+})

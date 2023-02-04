faa011431f2b5b09051f50bd70c8f8bedae0e36f
saica.go <zephyird@gmail.com>
2023-02-05 02:53:57 +0800

    Add "show-banner" toggle to preview tool

--- source/js/light-dark-storage.js
+++ source/js/light-dark-storage.js
@@ -2,6 +2,12 @@ function lightDarkStorage() {
     let theme = localStorage.getItem('theme');
     if (theme) {
         document.documentElement.setAttribute('theme', theme);
-    }    
+    }
+    let showBanner = localStorage.getItem("showBanner");
+    if (showBanner == null || showBanner == undefined || showBanner == "true") {
+        document.documentElement.setAttribute('banner', true)
+    } else {
+        document.documentElement.setAttribute('banner', false)
+    }
 };
 lightDarkStorage();

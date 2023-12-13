aa9651a25a6e8c27270905ceffe8494fe36a4d60
saicaca <zephyird@gmail.com>
2023-12-13 22:48:25 +0800

    fix: fix categories widget for Hexo 7.0.0 (article count is still broken) #43

--- scripts/categories-process.js
+++ scripts/categories-process.js
@@ -5,10 +5,9 @@ function build_tag_tree(tree, currDepth, maxDepth) {
     for(let item of tree) {
         ret += `
             <a class="category-link" href="${(url_for(item.path))}">
-                ${item.name}
-                <div class="category-count">${item.length}</div>
-            </a>
-        `;
+                ${item.name}`;
+        if (item.length) ret += `<div class="category-count">${item.length}</div>`;
+        ret += `</a>`;
         if ('children' in item && item.children.length>0 && currDepth < maxDepth) {
             ret += '<div class="children">'
             ret += build_tag_tree(item.children, currDepth+1, maxDepth);
@@ -38,15 +37,22 @@ function list_to_tree(list) {
     return roots;
 }
 
+function map_to_list(obj) {
+    if (Array.isArray(obj)) {       // For Hexo < 7.0.0
+        return obj;
+    }
+    return Object.values(obj);      // For Hexo >= 7.0.0
+}
+
 hexo.on('generateAfter', function(post){
     let depth = Infinity;
     // hexo.config.theme_config 更改为 hexo.theme，不然会报错 theme_config 不存在
     if (hexo.theme.categories && hexo.theme.categories.max_depth
             && hexo.theme.categories.max_depth > 0) {
         depth = hexo.theme.categories.max_depth;
-        console.log("depth: " + depth);
     }
-    let tree = list_to_tree(hexo.locals.get("categories").data);
+    let list = map_to_list(hexo.locals.get("categories").data);
+    let tree = list_to_tree(list);
     let cate_tree = build_tag_tree(tree, 1, depth);
     hexo.extend.helper.register('categories_tree', function() {
         return cate_tree;
